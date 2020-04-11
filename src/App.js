b1901727a4a0fea0a88c8c4334928ca65d50148b
Banphot Uthaphan <banphotuthaphan@Banphots-MBP.hub>
2020-04-11 17:16:05 +1200

    collection-item/ collection-preview components created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/hompage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
